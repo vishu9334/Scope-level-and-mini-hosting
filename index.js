d60b9392d30154377dd03cfe137956d3f300bca9
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-02-07 18:13:19 +0000

    21 video pt1

---
+++ index.js
@@ -0,0 +1,22 @@
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+if(true) {
+    const username = "vishal"
+    if(username === "vishal"){
+        const website = "yputube "
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ interesting +++++++++++++++++++++
