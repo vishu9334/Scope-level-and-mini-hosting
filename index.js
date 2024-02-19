db0f0e372e1dfb2e012603b5f4c5e2febe80bb8f
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-02-19 04:53:23 +0000

    change

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-function one(){
-    const username = "hitesh"
+// function one(){
+//     const username = "hitesh"
 
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
-    // two()
-}
-// one()
-if(true) {
-    const username = "vishal"
-    if(username === "vishal"){
-        const website = "yputube "
-        console.log(username + website);
-    }
-    // console.log(website);
-}
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     // two()
+// }
+// // one()
+// if(true) {
+//     const username = "vishal"
+//     if(username === "vishal"){
+//         const website = "yputube "
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+// }
 // console.log(username);
 
 // ++++++++++++++++++++++++++++ interesting +++++++++++++++++++++
+
+
+    console.log(addOne(2))
+    function addOne(num){
+       return num + 1
+}
+
+
+
+    const addTwo = function(num){ // This can say expression 
+    // and it is called hoisting 
+        return num + 3
+    }
+    addTwo(5)
